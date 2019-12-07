3bef113bbaa49b5c1a7769bd44c6ee727af779c8
Connor Worley <cbworley@yelp.com>
2019-12-06 16:01:19 -0800

    Add FFI signatures/datatypes for remaining types

--- ni_fpga/src/ffi.rs
+++ ni_fpga/src/ffi.rs
@@ -49,4 +49,220 @@ extern "C" {
         array: *const bool,
         size: usize,
     ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadU8"]
+    pub fn ReadU8(
+        session: Session,
+        indicator: Offset,
+        value: *mut u8,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayU8"]
+    pub fn ReadArrayU8(
+        session: Session,
+        indicator: Offset,
+        array: *mut u8,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteU8"]
+    pub fn WriteU8(
+        session: Session,
+        indicator: Offset,
+        value: u8,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayU8"]
+    pub fn WriteArrayU8(
+        session: Session,
+        indicator: Offset,
+        array: *const u8,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadU16"]
+    pub fn ReadU16(
+        session: Session,
+        indicator: Offset,
+        value: *mut u16,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayU16"]
+    pub fn ReadArrayU16(
+        session: Session,
+        indicator: Offset,
+        array: *mut u16,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteU16"]
+    pub fn WriteU16(
+        session: Session,
+        indicator: Offset,
+        value: u16,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayU16"]
+    pub fn WriteArrayU16(
+        session: Session,
+        indicator: Offset,
+        array: *const u16,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadU32"]
+    pub fn ReadU32(
+        session: Session,
+        indicator: Offset,
+        value: *mut u32,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayU32"]
+    pub fn ReadArrayU32(
+        session: Session,
+        indicator: Offset,
+        array: *mut u32,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteU32"]
+    pub fn WriteU32(
+        session: Session,
+        indicator: Offset,
+        value: u32,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayU32"]
+    pub fn WriteArrayU32(
+        session: Session,
+        indicator: Offset,
+        array: *const u32,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadU64"]
+    pub fn ReadU64(
+        session: Session,
+        indicator: Offset,
+        value: *mut u64,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayU64"]
+    pub fn ReadArrayU64(
+        session: Session,
+        indicator: Offset,
+        array: *mut u64,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteU64"]
+    pub fn WriteU64(
+        session: Session,
+        indicator: Offset,
+        value: u64,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayU64"]
+    pub fn WriteArrayU64(
+        session: Session,
+        indicator: Offset,
+        array: *const u64,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadI8"]
+    pub fn ReadI8(
+        session: Session,
+        indicator: Offset,
+        value: *mut i8,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayI8"]
+    pub fn ReadArrayI8(
+        session: Session,
+        indicator: Offset,
+        array: *mut i8,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteI8"]
+    pub fn WriteI8(
+        session: Session,
+        indicator: Offset,
+        value: i8,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayI8"]
+    pub fn WriteArrayI8(
+        session: Session,
+        indicator: Offset,
+        array: *const i8,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadI16"]
+    pub fn ReadI16(
+        session: Session,
+        indicator: Offset,
+        value: *mut i16,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayI16"]
+    pub fn ReadArrayI16(
+        session: Session,
+        indicator: Offset,
+        array: *mut i16,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteI16"]
+    pub fn WriteI16(
+        session: Session,
+        indicator: Offset,
+        value: i16,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayI16"]
+    pub fn WriteArrayI16(
+        session: Session,
+        indicator: Offset,
+        array: *const i16,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadI32"]
+    pub fn ReadI32(
+        session: Session,
+        indicator: Offset,
+        value: *mut i32,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayI32"]
+    pub fn ReadArrayI32(
+        session: Session,
+        indicator: Offset,
+        array: *mut i32,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteI32"]
+    pub fn WriteI32(
+        session: Session,
+        indicator: Offset,
+        value: i32,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayI32"]
+    pub fn WriteArrayI32(
+        session: Session,
+        indicator: Offset,
+        array: *const i32,
+        size: usize,
+    ) -> Status;
+
+    #[link_name = "NiFpgaDll_ReadI64"]
+    pub fn ReadI64(
+        session: Session,
+        indicator: Offset,
+        value: *mut i64,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_ReadArrayI64"]
+    pub fn ReadArrayI64(
+        session: Session,
+        indicator: Offset,
+        array: *mut i64,
+        size: usize,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteI64"]
+    pub fn WriteI64(
+        session: Session,
+        indicator: Offset,
+        value: i64,
+    ) -> Status;
+    #[link_name = "NiFpgaDll_WriteArrayI64"]
+    pub fn WriteArrayI64(
+        session: Session,
+        indicator: Offset,
+        array: *const i64,
+        size: usize,
+    ) -> Status;
 }
