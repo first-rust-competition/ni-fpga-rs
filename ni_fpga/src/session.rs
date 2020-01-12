b1996cbb4c901e9d11b11c3ad2785916c6494b75
Connor Worley <connorbworley@gmail.com>
2020-01-11 19:39:01 -0800

    Fix clippy warnings
    Fixes: #2

--- ni_fpga/src/session.rs
+++ ni_fpga/src/session.rs
@@ -16,18 +16,21 @@ impl Session {
         resource: &str,
     ) -> Result<Self, Status> {
         let mut handle: ffi::Session = Default::default();
+        let c_bitfile = CString::new(bitfile).unwrap();
+        let c_signature = CString::new(signature).unwrap();
+        let c_resource = CString::new(resource).unwrap();
         let status = Status::from(unsafe {
             ffi::Open(
-                CString::new(bitfile).unwrap().as_ptr(),
-                CString::new(signature).unwrap().as_ptr(),
-                CString::new(resource).unwrap().as_ptr(),
+                c_bitfile.as_ptr(),
+                c_signature.as_ptr(),
+                c_resource.as_ptr(),
                 0,
                 &mut handle as *mut ffi::Session,
             )
         });
         match status {
             Status::Success => Ok(Session {
-                handle: handle,
+                handle,
             }),
             _ => Err(status),
         }
